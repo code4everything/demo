b70bd56032f9a4f4f9c7e13a2d6f793f73fb3c27
zhazhapan <tao@zhazhapan.com>
2018-07-06 19:53:51 +0800

    redis demo for spring boot

--- demo-springboot/demo-springboot-redis/src/main/java/com/zhazhapan/demo/springboot/redis/DemoSpringbootRedisApplication.java
+++ demo-springboot/demo-springboot-redis/src/main/java/com/zhazhapan/demo/springboot/redis/DemoSpringbootRedisApplication.java
@@ -3,6 +3,9 @@ package com.zhazhapan.demo.springboot.redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pantao
+ */
 @SpringBootApplication
 public class DemoSpringbootRedisApplication {
 
--- demo-springboot/demo-springboot-redis/src/test/java/com/zhazhapan/demo/springboot/redis/DemoSpringbootRedisApplicationTests.java
+++ demo-springboot/demo-springboot-redis/src/test/java/com/zhazhapan/demo/springboot/redis/DemoSpringbootRedisApplicationTests.java
@@ -1,16 +1,41 @@
 package com.zhazhapan.demo.springboot.redis;
 
+import com.zhazhapan.demo.springboot.redis.entity.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoSpringbootRedisApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+    @Autowired
+    private RedisTemplate<String, User> redisTemplate;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void contextLoads() {}
+
+    @Test
+    public void testRedis() {
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+        User user = new User("redis", 7);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        user = new User("memchache", 8);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        user = new User("ehchache", 9);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+        assert 7 == redisTemplate.opsForValue().get("redis").getAge();
+        assert 8 == redisTemplate.opsForValue().get("memchahe").getAge();
+        assert 9 == redisTemplate.opsForValue().get("ehchache").getAge();
+    }
 }
