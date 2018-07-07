1336a856070e1047ed9c4aea7eea67c88ceeafa7
zhazhapan <tao@zhazhapan.com>
2018-07-07 22:16:09 +0800

    add author

--- demo-springboot/demo-springboot-security/src/main/java/com/zhazhapan/demo/springboot/security/DemoSpringbootSecurityApplication.java
+++ demo-springboot/demo-springboot-security/src/main/java/com/zhazhapan/demo/springboot/security/DemoSpringbootSecurityApplication.java
@@ -3,6 +3,9 @@ package com.zhazhapan.demo.springboot.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pantao
+ */
 @SpringBootApplication
 public class DemoSpringbootSecurityApplication {
 
