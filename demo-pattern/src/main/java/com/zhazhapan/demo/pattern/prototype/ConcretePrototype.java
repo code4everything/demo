fb2af214ead085ae6a495ddd6ba0c3caffe41f96
zhazhapan <tao@zhazhapan.com>
2018-07-22 11:24:52 +0800

    learn prototype pattern

---
+++ demo-pattern/src/main/java/com/zhazhapan/demo/pattern/prototype/ConcretePrototype.java
@@ -0,0 +1,13 @@
+package com.zhazhapan.demo.pattern.prototype;
+
+/**
+ * @author pantao
+ * @since 2018-07-22
+ */
+public class ConcretePrototype extends BasePrototype {
+
+    @Override
+    public void show() {
+        System.out.println("原型模式实现类");
+    }
+}
---
+++ demo-pattern/src/main/java/com/zhazhapan/demo/pattern/prototype/PrototypeDemo.java
@@ -0,0 +1,14 @@
+package com.zhazhapan.demo.pattern.prototype;
+
+/**
+ * @author pantao
+ * @since 2018-07-22
+ */
+public class PrototypeDemo {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BasePrototype prototype = new ConcretePrototype();
+        BasePrototype basePrototype = (BasePrototype) prototype.clone();
+        basePrototype.show();
+    }
+}
