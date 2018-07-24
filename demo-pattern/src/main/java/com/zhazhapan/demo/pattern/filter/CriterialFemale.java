c930e6e540783c8a797f9d265ae2c34a89c05b8e
zhazhapan <tao@zhazhapan.com>
2018-07-24 23:01:50 +0800

    learn filter pattern

---
+++ demo-pattern/src/main/java/com/zhazhapan/demo/pattern/filter/CriterialFemale.java
@@ -0,0 +1,22 @@
+package com.zhazhapan.demo.pattern.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author pantao
+ * @since 2018-07-24
+ */
+public class CriterialFemale implements Criteria {
+
+    @Override
+    public List<Person> meetCriteria(List<Person> personList) {
+        List<Person> femalePersons = new ArrayList<Person>();
+        for (Person person : personList) {
+            if ("MALE".equalsIgnoreCase(person.getGender())) {
+                femalePersons.add(person);
+            }
+        }
+        return femalePersons;
+    }
+}
