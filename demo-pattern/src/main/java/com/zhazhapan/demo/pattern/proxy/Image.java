51e0e554761bc477933ad7051fe0dce491908b21
zhazhapan <tao@zhazhapan.com>
2018-07-25 20:57:30 +0800

    learn proxy pattern

---
+++ demo-pattern/src/main/java/com/zhazhapan/demo/pattern/proxy/Image.java
@@ -0,0 +1,13 @@
+package com.zhazhapan.demo.pattern.proxy;
+
+/**
+ * @author pantao
+ * @since 2018-07-25
+ */
+public interface Image {
+
+    /**
+     * 显示
+     */
+    void display();
+}
---
+++ demo-pattern/src/main/java/com/zhazhapan/demo/pattern/proxy/ProxyImage.java
@@ -0,0 +1,24 @@
+package com.zhazhapan.demo.pattern.proxy;
+
+/**
+ * @author pantao
+ * @since 2018-07-25
+ */
+public class ProxyImage implements Image {
+
+    private RealImage realImage;
+
+    private String fileName;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
