d848678cdc4dd94aea6676e672d61763bb97222e
zhazhapan <tao@zhazhapan.com>
2018-07-31 21:30:16 +0800

    test lombok

---
+++ demo-test/src/main/java/com/zhazhapan/demo/test/model/Test.java
@@ -0,0 +1,19 @@
+package com.zhazhapan.demo.test.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author pantao
+ * @since 2018-07-31
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+
+    private String id;
+
+    private String name;
+}
