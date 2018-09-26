cedaea6c8ce0873416b19256249dbe58e9b46984
pantao <tao@zhazhapan.com>
2018-09-26 22:59:05 +0800

    finish in order and post order binary tree

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/BinaryTree.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/BinaryTree.java
@@ -20,8 +20,43 @@ public class BinaryTree {
         TreeNode right = new TreeNode(7);
         root.left = left;
         root.right = right;
+        // 前序遍历
         List<Integer> preOrder = new ArrayList<Integer>(8) {{addAll(Arrays.asList(9, 8, 7));}};
         Assert.assertEquals(preOrder, binaryTree.preOrderTraversal(root));
+        // 中序遍历
+        List<Integer> inOrder = new ArrayList<Integer>(8) {{addAll(Arrays.asList(8, 9, 7));}};
+        Assert.assertEquals(inOrder, binaryTree.inOrderTraversal(root));
+        // 后序遍历
+        List<Integer> postOrder = new ArrayList<Integer>(8) {{addAll(Arrays.asList(8, 7, 9));}};
+        Assert.assertEquals(postOrder, binaryTree.postOrderTraversal(root));
+    }
+
+    public List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            if (root.left != null) {
+                list.addAll(postOrderTraversal(root.left));
+            }
+            if (root.right != null) {
+                list.addAll(postOrderTraversal(root.right));
+            }
+            list.add(root.val);
+        }
+        return list;
+    }
+
+    public List<Integer> inOrderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            if (root.left != null) {
+                list.addAll(inOrderTraversal(root.left));
+            }
+            list.add(root.val);
+            if (root.right != null) {
+                list.addAll(inOrderTraversal(root.right));
+            }
+        }
+        return list;
     }
 
     public List<Integer> preOrderTraversal(TreeNode root) {
