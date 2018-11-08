88e20f47b3415664103d1e53c2864a52b5a21c1b
pantao <tao@zhazhapan.com>
2018-11-08 18:48:53 +0800

    路径总和

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/BinaryTree.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/BinaryTree.java
@@ -52,6 +52,27 @@ public class BinaryTree {
         Assert.assertTrue(binaryTree.isSymmetric(root));
         root.right.right.val = 7;
         Assert.assertFalse(binaryTree.isSymmetric(root));
+        // 路径总和
+        Assert.assertFalse(binaryTree.hasPathSum(root, 89));
+        Assert.assertTrue(binaryTree.hasPathSum(root, 15));
+    }
+
+    /**
+     * 路径总和
+     *
+     * @param root {@link TreeNode}
+     * @param sum 目标和
+     *
+     * @return 是否存在一条路径和等于目标和
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.right, sum - root.val) || hasPathSum(root.left, sum - root.val);
     }
 
     /**
