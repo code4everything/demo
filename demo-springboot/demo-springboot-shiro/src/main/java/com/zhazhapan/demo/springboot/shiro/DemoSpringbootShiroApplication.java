f81f6cedd11552379f7b623577174b821067b22f
pantao <tao@zhazhapan.com>
2018-12-09 21:57:26 +0800

    新建SpringBootShiro项目

---
+++ demo-springboot/demo-springboot-shiro/src/main/java/com/zhazhapan/demo/springboot/shiro/DemoSpringbootShiroApplication.java
@@ -0,0 +1,16 @@
+package com.zhazhapan.demo.springboot.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pantao
+ * @since 2018-12-09
+ */
+@SpringBootApplication
+public class DemoSpringbootShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootShiroApplication.class, args);
+    }
+}
---
+++ demo-springboot/demo-springboot-shiro/src/test/java/com/zhazhapan/demo/springboot/shiro/DemoSpringbootShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhazhapan.demo.springboot.shiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringbootShiroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
