5a4842e505e00198c127449d9550175375be3463
pantao <tao@zhazhapan.com>
2018-12-12 20:20:44 +0800

    搜索二叉搜索树

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/tree/BinarySearchTree.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/tree/BinarySearchTree.java
@@ -8,6 +8,19 @@ import com.zhazhapan.demo.algorithm.leetcode.model.TreeNode;
  */
 public class BinarySearchTree {
 
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (root.val > val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
--- demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/tree/BinarySearchTreeTest.java
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/tree/BinarySearchTreeTest.java
@@ -1,5 +1,6 @@
 package com.zhazhapan.demo.algorithm.leetcode.tree;
 
+import com.zhazhapan.demo.algorithm.leetcode.model.TreeNode;
 import com.zhazhapan.demo.algorithm.leetcode.tree.binary.TreeCodec;
 import org.junit.Test;
 
@@ -14,5 +15,14 @@ public class BinarySearchTreeTest {
         assert !tree.isValidBST(codec.deserialize("10,5,(15,6,20)"));
         assert tree.isValidBST(codec.deserialize("-2147483648,null,2147483647"));
 
+    }
+
+    @Test
+    public void searchBST() {
+        TreeCodec codec = new TreeCodec();
+        BinarySearchTree tree = new BinarySearchTree();
+        TreeNode root = codec.deserialize("4,(2,1,3),7");
+        assert "(2,1,3)".equals(codec.serialize(tree.searchBST(root, 2)));
+        assert "".equals(codec.serialize(tree.searchBST(root, 5)));
     }
 }
