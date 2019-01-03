4a7826fb02ece424d55af4c4ea7a768f25aef0a3
pantao <tao@zhazhapan.com>
2019-01-03 19:15:23 +0800

    x的平方根

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/binary/search/Searcher.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/binary/search/Searcher.java
@@ -6,6 +6,32 @@ package com.zhazhapan.demo.algorithm.leetcode.binary.search;
  */
 public class Searcher {
 
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        } else if (x == 1) {
+            return 1;
+        } else {
+            long target = 0;
+            int left = 1;
+            int right = x / 2;
+            while (left <= right) {
+                target = (left + right) / 2;
+                long sqrt = target * target;
+                if (sqrt == x) {
+                    break;
+                } else if (sqrt > x) {
+                    right = (int) target - 1;
+                } else if ((target + 1) * (target + 1) > x) {
+                    break;
+                } else {
+                    left = (int) (target + 1);
+                }
+            }
+            return (int) target;
+        }
+    }
+
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
--- demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/binary/search/SearcherTest.java
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/binary/search/SearcherTest.java
@@ -10,4 +10,17 @@ public class SearcherTest {
         assert 4 == searcher.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
         assert -1 == searcher.search(new int[]{-1, 0, 3, 5, 9, 12}, 2);
     }
+
+    @Test
+    public void mySqrt() {
+        Searcher searcher = new Searcher();
+        assert 0 == searcher.mySqrt(0);
+        assert 1 == searcher.mySqrt(1);
+        assert 1 == searcher.mySqrt(2);
+        assert 2 == searcher.mySqrt(4);
+        assert 4 == searcher.mySqrt(16);
+        assert 4 == searcher.mySqrt(24);
+        assert 6 == searcher.mySqrt(36);
+        assert searcher.mySqrt(2147395599) == 46339;
+    }
 }
