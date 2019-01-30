a44f0a5e6ee00d4372b75cafcb9a704a292bd685
pantao <tao@zhazhapan.com>
2019-01-30 15:05:21 +0800

    打开转盘锁

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/queue/Solution.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/queue/Solution.java
@@ -1,10 +1,56 @@
 package com.zhazhapan.demo.algorithm.leetcode.queue;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author pantao
  * @since 2019/1/29
  **/
 public class Solution {
+
+    public int openLock(String[] deadends, String target) {
+        final String defaultLock = "0000";
+        Set<String> visited = new HashSet<>();
+        for (String deadend : deadends) {
+            if (deadend.equals(defaultLock)) {
+                return -1;
+            }
+            visited.add(deadend);
+        }
+        visited.add(defaultLock);
+        List<String> preList = new ArrayList<>();
+        preList.add(defaultLock);
+        List<String> list;
+        int res = 0;
+        while (!preList.isEmpty()) {
+            list = new ArrayList<>();
+            for (int k = 0; k < preList.size(); k++) {
+                String lock = preList.get(k);
+                if (lock.equals(target)) {
+                    return res;
+                }
+                for (int i = 0; i < 4; i++) {
+                    char[] chars = lock.toCharArray();
+                    char[] cs = new char[]{chars[i] == '0' ? '9' : (char) (chars[i] - 1), chars[i] == '9' ? '0' :
+                            (char) (chars[i] + 1)};
+                    for (int j = 0; j < 2; j++) {
+                        chars[i] = cs[j];
+                        String newLock = String.valueOf(chars);
+                        if (!visited.contains(newLock)) {
+                            visited.add(newLock);
+                            list.add(newLock);
+                        }
+                    }
+                }
+            }
+            preList = list;
+            res++;
+        }
+        return -1;
+    }
 
     public int numIslands(char[][] grid) {
         int nums = 0;
--- demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/queue/SolutionTest.java
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/queue/SolutionTest.java
@@ -13,4 +13,13 @@ public class SolutionTest {
         assert solution.numIslands(new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1',
                 '0', '0'}, {'0', '0', '0', '1', '1'}}) == 3;
     }
+
+    @Test
+    public void openLock() {
+        assert solution.openLock(new String[]{"0201", "0101", "0102", "1212", "2002"}, "0202") == 6;
+        assert solution.openLock(new String[]{"8888"}, "0009") == 1;
+        assert solution.openLock(new String[]{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"},
+                "8888") == -1;
+        assert solution.openLock(new String[]{"0000"}, "8888") == -1;
+    }
 }
