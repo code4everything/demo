f2956b4a3a81a475b1606651f52cb3cc6509e8e8
pantao <tao@zhazhapan.com>
2019-02-03 10:48:45 +0800

    查询后的偶数和

---
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/contest/Contest985Test.java
@@ -0,0 +1,14 @@
+package com.zhazhapan.demo.algorithm.leetcode.contest;
+
+import cn.hutool.core.lang.Console;
+import org.junit.Test;
+
+public class Contest985Test {
+
+    @Test
+    public void test() {
+        Contest985 contest985 = new Contest985();
+        Console.log(contest985.sumEvenAfterQueries(new int[]{1, 2, 3, 4}, new int[][]{{1, 0}, {-3, 1}, {-4, 0}, {2,
+                3}}));
+    }
+}
