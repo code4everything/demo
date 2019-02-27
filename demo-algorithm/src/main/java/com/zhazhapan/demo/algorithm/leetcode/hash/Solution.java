7a8edff8d3c2f48d340523e8670c2289f9944316
pantao <tao@zhazhapan.com>
2019-02-27 14:08:29 +0800

    只出现一次的数字

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/hash/Solution.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/hash/Solution.java
@@ -8,6 +8,14 @@ import java.util.Arrays;
  **/
 public class Solution {
 
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int i = 0; i < nums.length; i++) {
+            single ^= nums[i];
+        }
+        return single;
+    }
+
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
