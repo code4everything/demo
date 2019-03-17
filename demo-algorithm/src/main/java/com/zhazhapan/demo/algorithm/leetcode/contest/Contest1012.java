1f5726d52fc92d7a3cd994b09583fe93480d9fd0
pantao <easepan@qq.com>
2019-03-17 10:48:43 +0800

    十进制整数的补码

---
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/contest/Contest1012.java
@@ -0,0 +1,18 @@
+package com.zhazhapan.demo.algorithm.leetcode.contest;
+
+/**
+ * @author pantao
+ * @since 2019-03-17
+ */
+public class Contest1012 {
+
+    public int bitwiseComplement(int n) {
+        int bit = 1;
+        int nc = n;
+        while (nc > 1 || nc < -1) {
+            nc >>>= 1;
+            bit = (bit << 1) + 1;
+        }
+        return n ^ bit;
+    }
+}
