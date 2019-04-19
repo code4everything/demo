6942ac26162ce1cdc0092d9ebe8eff6b03b1ae88
pantao <easepan@qq.com>
2019-04-19 13:35:33 +0800

    扁平化嵌套列表迭代器

---
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/design/NestedIteratorTest.java
@@ -0,0 +1,14 @@
+package com.zhazhapan.demo.algorithm.leetcode.design;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NestedIteratorTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
