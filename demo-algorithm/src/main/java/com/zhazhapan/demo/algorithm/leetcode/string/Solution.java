75dd87b9663b3d16d0f3d9c291fda526e6b7a99b
pantao <easepan@qq.com>
2019-05-09 20:26:46 +0800

    单词接龙

--- demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/string/Solution.java
+++ demo-algorithm/src/main/java/com/zhazhapan/demo/algorithm/leetcode/string/Solution.java
@@ -3,10 +3,7 @@ package com.zhazhapan.demo.algorithm.leetcode.string;
 import com.zhazhapan.demo.algorithm.common.annotation.LeetCode;
 import com.zhazhapan.demo.algorithm.common.enums.Difficulty;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author pantao
@@ -21,6 +18,47 @@ public class Solution {
     private final char zero = '0';
 
     private final char nine = '9';
+
+    @LeetCode(id = 127, title = "单词接龙", difficulty = Difficulty.MEDIUM)
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int ans = 0;
+        boolean[] hashset = new boolean[wordList.size()];
+        Deque<String> queue = new ArrayDeque();
+        queue.offer(beginWord);
+        for (int i = 0; i < wordList.size(); i++) {
+            if (wordList.get(i).equals(beginWord)) {
+                hashset[i] = true;
+            }
+        }
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            ans++;
+            for (int i = 0; i < n; i++) {
+                String cur = queue.poll();
+                if (cur.equals(endWord)) {
+                    return ans;
+                }
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (!hashset[j] && isLegal(cur, wordList.get(j))) {
+                        hashset[j] = true;
+                        queue.offer(wordList.get(j));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private boolean isLegal(String s1, String s2) {
+        int sum = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                sum++;
+            }
+        }
+        return sum == 1;
+    }
 
     @LeetCode(id = 3, title = "无重复字符的最长子串", difficulty = Difficulty.MEDIUM)
     public int lengthOfLongestSubstring(String s) {
