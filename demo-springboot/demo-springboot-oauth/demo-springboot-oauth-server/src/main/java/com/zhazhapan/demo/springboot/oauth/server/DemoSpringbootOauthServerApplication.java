ea7967c5f3121cd54cabb2fbc255cb0ef3fb3773
pantao <easepan@qq.com>
2019-06-03 13:46:43 +0800

    new springboot oauth demo

---
+++ demo-springboot/demo-springboot-oauth/demo-springboot-oauth-server/src/main/java/com/zhazhapan/demo/springboot/oauth/server/DemoSpringbootOauthServerApplication.java
@@ -0,0 +1,16 @@
+package com.zhazhapan.demo.springboot.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pantao
+ * @since 2019/06/03
+ */
+@SpringBootApplication
+public class DemoSpringbootOauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootOauthServerApplication.class, args);
+    }
+}
