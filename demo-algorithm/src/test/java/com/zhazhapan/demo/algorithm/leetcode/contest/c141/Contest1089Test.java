48931c17b7d4af24a5f5fb919032ede9592d6bf6
pantao <tao@zhazhapan.com>
2019-06-16 10:59:58 +0800

    feat: complete contest 1089

---
+++ demo-algorithm/src/test/java/com/zhazhapan/demo/algorithm/leetcode/contest/c141/Contest1089Test.java
@@ -0,0 +1,13 @@
+package com.zhazhapan.demo.algorithm.leetcode.contest.c141;
+
+import org.junit.Test;
+
+public class Contest1089Test {
+
+    @Test
+    public void duplicateZeros() {
+        Contest1089 contest1089 = new Contest1089();
+        int[] arr = new int[]{0, 0, 0, 0, 0, 0, 0};
+        contest1089.duplicateZeros(arr);
+    }
+}
