1f8f0e6959858a622eac48a6d20a8d1d0192b01a
pantao <easepan@qq.com>
2023-04-30 09:49:03 +0800

    feat: leetcode problem 1033

--- demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/math/SolutionKt.kt
+++ demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/math/SolutionKt.kt
@@ -9,4 +9,14 @@ class SolutionKt {
     fun smallestEvenMultiple(n: Int): Int {
         return if (n and 1 == 1) n shl 1 else n
     }
+
+    @LeetCode(id = 1033, title = "移动石子直到连续", difficulty = Difficulty.MEDIUM)
+    fun numMovesStones(a: Int, b: Int, c: Int): IntArray {
+        val min = minOf(a, minOf(b, c))
+        val max = maxOf(a, maxOf(b, c))
+        val mid = a + b + c - min - max
+        val maxAns = max - min - 2
+        val minAns = if (maxAns == 0) 0 else if (max - mid < 3 || mid - min < 3) 1 else 2
+        return intArrayOf(minAns, maxAns)
+    }
 }
