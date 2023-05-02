760a62aba2dd04d178f9ef5d16ee042aee4cb78e
pantao <easepan@qq.com>
2023-05-02 19:48:10 +0800

    feat: leetcode problem 970

--- demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/math/SolutionKt.kt
+++ demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/math/SolutionKt.kt
@@ -5,6 +5,41 @@ import org.code4everything.demo.algorithm.common.enums.Difficulty
 
 class SolutionKt {
 
+    @LeetCode(id = 970, difficulty = Difficulty.MEDIUM, title = "强整数")
+    fun powerfulIntegers(x: Int, y: Int, bound: Int): List<Int> {
+        val powX = powInts(x, bound)
+        val powY = powInts(y, bound)
+        val result = hashSetOf<Int>()
+
+        powX.forEach { xx ->
+            powY.forEach for2@{ yy ->
+                val sum = xx + yy
+                if (sum > bound) {
+                    return@for2
+                } else {
+                    result.add(sum)
+                }
+            }
+        }
+
+        return result.toList()
+    }
+
+    private fun powInts(x: Int, bound: Int): List<Int> {
+        if (x == 1) {
+            return listOf(1)
+        }
+
+        val list = arrayListOf(1, x)
+        var pow = x * x
+        while (pow <= bound) {
+            list.add(pow)
+            pow *= x
+        }
+
+        return list
+    }
+
     @LeetCode(id = 2413, title = "最小偶倍数", difficulty = Difficulty.EASY)
     fun smallestEvenMultiple(n: Int): Int {
         return if (n and 1 == 1) n shl 1 else n
