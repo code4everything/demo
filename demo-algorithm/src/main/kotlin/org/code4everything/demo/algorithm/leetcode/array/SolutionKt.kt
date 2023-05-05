cfcd1a752709a677206954ee7f2d3850b818f4d6
pantao <easepan@qq.com>
2023-05-05 11:23:42 +0800

    feat: leetcode problem 2432

--- demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/array/SolutionKt.kt
+++ demo-algorithm/src/main/kotlin/org/code4everything/demo/algorithm/leetcode/array/SolutionKt.kt
@@ -5,6 +5,25 @@ import org.code4everything.demo.algorithm.common.enums.Difficulty
 
 class SolutionKt {
 
+    @LeetCode(id = 2432, title = "处理用时最长的那个任务的员工", difficulty = Difficulty.EASY)
+    fun hardestWorker(n: Int, logs: Array<IntArray>): Int {
+        var startAt = 0
+        var maxTime = 0
+        var minId = Int.MAX_VALUE
+        logs.forEach {
+            val id = it.first()
+            val time = it.last() - startAt
+            startAt = it.last()
+            if (time > maxTime) {
+                maxTime = time
+                minId = id
+            } else if (time == maxTime && id < minId) {
+                minId = id
+            }
+        }
+        return minId
+    }
+
     @LeetCode(id = 228, title = "汇总区间", difficulty = Difficulty.EASY)
     fun summaryRanges(nums: IntArray): List<String> {
         if (nums.isEmpty()) {
